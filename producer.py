97c306007953d0211703eaef5f29d1444494d5f3
Yash Sampat <yashsampat23154@gmail.com>
2023-03-14 11:14:29 +0530

    Updated : monitoring.py and producer.py
    monitory.py : Added comments to make code for readable.
    producer.py : Added entire code within a function which will be called by a python thread.

--- producer.py
+++ producer.py
@@ -3,18 +3,21 @@ from time import sleep
 import json 
 import time
 
-subsystem=input("Enter your subsystem name: ")
-id=input("Enter your instance id: ")
-topic_name='monitor_topic'
-kafka_ip='localhost'
-kafka_port='9092'
-producer = KafkaProducer(bootstrap_servers=[kafka_ip+":"+kafka_port],api_version=(0, 10, 1))
-# conn="connection built"
-# producer.send(topic_name, json.dumps(conn).encode('utf-8'))
 
-while True:
-    t=time.time()
-    message=subsystem+":"+id+":"+str(t)
-    producer.send(topic_name, json.dumps(message).encode('utf-8'))
-    sleep(60)
-    print(message)
+def sendheartBeat(subsystemName, subsystemInstanceId) : 
+
+    kafkaIp = "10.2.133.16"
+    kafkaPortNo = "9092"
+    kafkaTopicName='heartbeatMonitoring'
+    
+    producer = KafkaProducer(bootstrap_servers=[kafkaIp+":"+kafkaPortNo],api_version=(0, 10, 1))
+
+    while True:
+        
+        currentTime = time.time()
+        message=subsystemName+":"+subsystemInstanceId+":"+str(currentTime)
+        
+        producer.send(kafkaTopicName, json.dumps(message).encode('utf-8'))
+        
+        sleep(60)
+        
