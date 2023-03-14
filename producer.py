f7b374479825d4ade49a7b631fa3ef26c53604f9
Yash Sampat <yashsampat23154@gmail.com>
2023-03-14 11:22:28 +0530

    Comments added in producer.py

--- producer.py
+++ producer.py
@@ -1,8 +1,18 @@
+# What does this code do : 
+
+# This code needs to be added within every subsystem so that it may keep sending its heartbeat to the Kafka Stream.
+
+# How to use this code : 
+
+# Step 1 : Add all the necessary imports 
+# Step 2 : Create a thread and run this function within the thread. 
+
+
+
 from kafka import KafkaProducer
 from time import sleep
 import json 
 import time
-
 
 def sendheartBeat(subsystemName, subsystemInstanceId) : 
 
