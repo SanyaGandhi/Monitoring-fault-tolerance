ad39b8b41899591a540d70b12f64cd1cbdb04b34
LOKESH SHARMA <lokeshsharma123456@gmail.com>
2023-04-10 13:57:20 +0530

    NEW CHANGES DONE COMPLETE. JUST CHANGE TIME
    ALL WORKING FINE.

--- heartbeatGenerator.py
+++ heartbeatGenerator.py
@@ -8,28 +8,39 @@
 # Step 2 : Create a thread and run this function within the thread. 
 
 
-
 from kafka import KafkaProducer
 from time import sleep
 import json 
 import time
 
-def sendheartBeat(subsystemName, subsystemInstanceId) : 
+def sendheartBeat(subsystemName, containerId, VMhost, VMuname, VMpswd) : 
 
-    kafkaIp = "localhost"
+    # kafkaIp = "20.106.92.171"
+    # kafkaPortNo = "9092"
+    kafkaIp = "0.0.0.0"
     kafkaPortNo = "9092"
     kafkaTopicName='heartbeatMonitoring'
     
     producer = KafkaProducer(bootstrap_servers=[kafkaIp+":"+kafkaPortNo],api_version=(0, 10, 1))
-
+    i = 1
     while True:
         
         currentTime = time.time()
-        message=subsystemName+":"+subsystemInstanceId+":"+str(currentTime)
-        
+        message=str(subsystemName)+":"+str(containerId)+":"+str(VMhost)+":"+str(VMuname)+":"+str(VMpswd)+":"+str(currentTime)
+        # print(message)
         producer.send(kafkaTopicName, json.dumps(message).encode('utf-8'))
-        
+        print("heartbeatsent",i)
+        i += 1
         sleep(30)
 
-#for testing purposes
-sendheartBeat('deployer3', '600') #comment out in actual implementation
+# Give the following parameters:
+# 1.  Subsystem Name 
+# 2. Container Id
+# 3. VM Hostname 
+# 4. VM username 
+# 5. VM password
+
+# For testing purposes
+subsystem = input("Enter subsystem name : ")
+containerId = input ("Enter contaner id : ")
+sendheartBeat(subsystem, containerId,'localhost', 'uname', 'pswd') #comment out in actual implementation
--- monitoring.py
+++ monitoring.py
@@ -2,7 +2,7 @@ from kafka import KafkaConsumer
 import json
 import threading
 import time
-from time import sleep 
+from time import sleep
 import logging
 import global_file
 import sys
@@ -14,148 +14,175 @@ import pymongo
 import json
 from pymongo import MongoClient
 
-#connect to python applicaiton
-client = pymongo.MongoClient("mongodb+srv://test:test@cluster0.xcykxcz.mongodb.net/?retryWrites=true&w=majority")
+# Connect to python applicaiton
+client = pymongo.MongoClient(
+    "mongodb+srv://test:test@cluster0.xcykxcz.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
-# create a new database called 'mydatabase'
+# print(client,db)
+db_names = client.list_database_names()
+# print(db_names)
+# create a new database
 mydb = client["IAS_PROJECT"]
 # i have created  it explicitly
 
-# create a new collection called 'mycollection' in the 'mydatabase' database
-mycol = mydb["Monitoring"]
+# Create a new collection called 'Monitoring' in the 'IAS_PROJECT' database
+monitoringCollection = mydb["Monitoring"]
 ##########################################################
 
 
 ##########    VARIABLE DECLARATIONS    ##########
-dict={}
+newDataDictionary = []
 
 # If the time since the last message received by service is > notifyTime then we'll send a message to the platform Admin via the notification service.
-notifyTime = 150
+notifyTime = 15
 
 # If the time since the last message received by service is > killTime then we'll pass the service along with its information to the Fault Tolerance system which will deal with it.
-killTime = 300
+killTime = 30
 
 # To connect to the kafka stream
-kafkaIp = "localhost"
+# kafkaIp = "20.106.92.171"
+# kafkaPortNo = "9092"
+kafkaIp = "0.0.0.0"
 kafkaPortNo = "9092"
 kafkaTopicName = "heartbeatMonitoring"
 kafkaGroupId = "Monitoring"
 
 # Log should be stored in logFile
 logFile = "Monitoring.log"
-logging.basicConfig(level=logging.WARNING, filename=logFile, filemode='w', format='%(asctime)s - [%(levelname)s] - %(filename)s:%(funcName)s:%(lineno)d - %(message)s')
+logging.basicConfig(level=logging.WARNING, filename=logFile, filemode='w',
+                    format='%(asctime)s - [%(levelname)s] - %(filename)s:%(funcName)s:%(lineno)d - %(message)s')
 
 ##########    THREAD FUNCTION IMPLEMENTATION    ##########
 
-def mongo_update():
-    #function for updating mongo entries using dict
-    #DO: update the mango db after connecting
-    #### i am assuming you are accepting this through kafka
-    updated_data = []
-    with open('ias.json', 'r') as f:
-        data = json.load(f)
-        for item in data:
-            item['epoc_val'] = time.time()
-            updated_data.append(item)
-    #### till then you have data through kafka
-    ## there may be updated entries and new entries
 
-    ## inserting data in result as SubsystemName_Subsytemid
-    # where _ is split 
-    result = {}
-    for item in data:
-        key = f"{item['Subsystem_NAme']}_{item['Sub_system_Id']}"
-        value = item['epoc_val']
-        result[key] = value
+def mongoUpdate():
+
+    print("Trying to update Mongo")
+    for subsystem in newDataDictionary:
+        #newDataDictinary contains new messages which have to be updated in mongo
         
-    ####breaker##########33
-    pprint(result)
-    print()
-    with open('ias.json', 'w') as f:
-        json.dump(updated_data, f)
-    # t = t- 1
-    # if t < 0:
-    #     break
-    ####breaker##########33
- 
-    # get collection from mongo db
-    old_doc = mycol.find_one()
+        # define a filter for the subsystem/doc with the specified name
+        filter = {"name": subsystem["name"]}#LoadBalancer_2
 
-    # Iterate over new values and update if they are greater than the old ones
-    for key, value in result.items():
-        if key in old_doc and value > old_doc[key]:
-            mycol.update_one({'_id': old_doc['_id']}, {'$set': {key: value}})
+        #check whether this subsystem already exists
+        existing_subsystem = monitoringCollection.find_one(filter)
+
+        # if the subsystem exists, update its epoctime field
+        if existing_subsystem is not None:
+            
+            update = {"$set": {"epoc_time": subsystem["epoc_time"]}}
+            monitoringCollection.update_one(filter, update)
+            print("Document updated.")
+        # if the document does not exist, insert a new subsystem as new document
         else:
-            old_doc[key] = value
+            monitoringCollection.insert_one(subsystem)
+            print(f"New subsystem {subsystem['name']} document inserted.")
 
-    # Check if there are any new key-value pairs in result that were not in old_values
-    new_pairs = set(result.items()) - set(old_doc.items())
-    if new_pairs:
-        # Add new key-value pairs to old_doc
-        for key, value in new_pairs:
-            old_doc[key] = value
-        # Insert updated document back into collection
-        mycol.replace_one({'_id': old_doc['_id']}, old_doc, upsert=True)
-    # time.sleep(5)
-    print("trying to update mongo")
-    
+        
 
 
 def isalive():
-    while(True) :
-        sleep(5)
-        mongo_update()
-        if(int(sys.argv[1])==global_file.globe):
-            #***********************************************88
-            dict2 = mycol.find_one()
-            #DO: copy data from mongo db into a dictionary 'dict2'
-            currentTime = time.time()
-            for k,vals in dict2.items():
-                #_id also created when we first time update
-                '''Performing an update on the path '_id'
-                  would modify the immutable field '_id', 
-                  full error: {'index': 0, 'code': 66, 'errmsg':
-                    "Performing an update on the path '_id'
-                      would modify the immutable field '_id'"}
-                '''
-                if k != '_id': ##for ignoring above error
-                    vals=float(vals)
-                    diff=currentTime-vals
-                    if diff>=15 and diff<30:
-                        logging.error('The subsystem with instance id = {} has been inactive since a long time'.format(k))
-                        print('time to notify the platform admin')
-                    if vals-currentTime>=45:
-                        logging.critical('The subsystem with instance id = {} needs to be killed'.format(k))
-                        print('time to notify kill the instance')
-                        #DO: delete the entry from mongo db
-                        mycol.update_one({"_id": dict2["_id"]}, {"$unset": {k: ""}})
+    while (True):
+        sleep(50)
+        mongoUpdate()
+        if (int(sys.argv[1]) == global_file.globe):
+            # ***********************************************
+            '''here notification and updation done on mongodb 
+            '''
+            #Notification when heartbeat time exceeds 15 and less than 30
+            # make filter on the basis of time difference
+            current_time = time.time()
+            filter_for_notification = {"$expr": {"$and": [
+                            {"$gt": [{"$subtract": [float(current_time), {"$toDouble": "$epoc_time"}]}, 30]},
+                            {"$lt": [{"$subtract": [float(current_time), {"$toDouble": "$epoc_time"}]}, 50]}
+                        ]}}
 
-            if int(sys.argv[1])==1:
-                global_file.globe=2
+            # retrieve all subsystems document that matches the filter
+            subsystems_to_notify = monitoringCollection.find(filter_for_notification)
+
+            # initialize a list to store the names of documents/ subsystems
+            subsystem_names = []
+
+            # iterate over the documents and append names of the documents to the list
+            for subsystem in subsystems_to_notify:
+                subsystem_names.append(subsystem["name"])
+                print("Platform developer have look at thenm",subsystem["name"],subsystem["containerId"] )
+
+            # print the list of document names
+            print(f"Documents with name {subsystem_names} have a time difference between 10 and 20 seconds.")
+
+
+
+            # **********************************************************************8
+            #Iterate and remove
+            current_time = time.time()
+            
+            #make filter on the basis of time exceeding 
+            filter_to_kill = {"$expr": {"$gt": [{"$subtract": [float(current_time), {"$toDouble": "$epoc_time"}]}, 60]}}
+            
+            # retrieve all subsystems documents that match the filter
+            documents_to_remove = monitoringCollection.find(filter_to_kill)
+
+            pprint(documents_to_remove)
+            # Delete all documents that match the filter
+            result = monitoringCollection.delete_many(filter_to_kill) 
+
+            # Print the number of documents removed
+            print(f"Removed {result.deleted_count} documents.")
+
+            if int(sys.argv[1]) == 1:
+                global_file.globe = 2
             else:
-                global_file.globe=1
+                global_file.globe = 1
 
 
-
-
-t = threading.Thread(target = isalive, args=[])
+t = threading.Thread(target=isalive, args=[])
 t.start()
-
 
 
 ##########    MONITORING SUBSYSTEM IMPLEMENTATION    ##########
 
 
-consumer = KafkaConsumer(kafkaTopicName, group_id=kafkaGroupId, bootstrap_servers=[kafkaIp+":"+kafkaPortNo])
-
+consumer = KafkaConsumer(
+    kafkaTopicName, 
+    group_id=kafkaGroupId,
+    bootstrap_servers=[f"{kafkaIp}:{kafkaPortNo}"])
+print("hi iam running")
 for message in consumer:
-    messageContents = message.value.decode('UTF-8').split(':')
-    messageContents[0] = messageContents[0][1:]
-    messageContents[2] = messageContents[2][:-1]
-    key=f'{messageContents[0]}:{messageContents[1]}'
-    dict[key]=messageContents[2]
-    logging.info('The subsystem = {} with instance id = {} has a new entry'.format(messageContents[0],messageContents[1],messageContents[2]))
-    print(dict)
+    message_value = message.value.decode('utf-8').strip('"')
+    print("********",message_value)
+    name, containerId, VMhost, VMuname, VMpswd, epoc_time= message_value.split(':')
+    print()
+    print()
+    # problem was newDAtaDictionary appending every time so we are deleteing and end entry 
+    # if same name and containerid present
+    
+    # *******************deleting old entry ***********************************
+    # Check if subsystem with same name and containerId already exists in newDataDictionary
+    subsystem_index = None
+    for index, subsystem in enumerate(newDataDictionary):
+        if subsystem['name'] == name and subsystem['containerId'] == containerId:
+            subsystem_index = index
+            break
 
-    ## my assumption is you are providing a code such as 
-    # SubsystemName_SubsytemId as key with splitter is underscore'_'
+    # If subsystem exists, remove it from newDataDictionary, so avoid duplicacy
+    if subsystem_index is not None:
+        del newDataDictionary[subsystem_index]
+    #*******************old endtry deleted ****************************************    
+        
+    #*******************insert new one ********************************************
+    subsystem = {
+        "name": name,
+        "containerId": containerId,
+        "VMhost": VMhost,
+        "VMuname": VMuname,
+        "VMpswd": VMpswd,
+        "epoc_time": epoc_time
+    }
+    newDataDictionary.append(subsystem)
+    #******************new entry inserted *****************************************
+    # logging.info('The subsystem = {} with instance id = {} has a new entry'.format(
+    #     messageContents[0], messageContents[1], messageContents[2]))
+    print(newDataDictionary)
+
+    
