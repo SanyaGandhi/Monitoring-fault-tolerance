58982d20d25dc61c91fa57ecfdf69945e5225122
LOKESH SHARMA <lokeshsharma123456@gmail.com>
2023-04-10 23:09:16 +0530

    global file as a document inserted

--- monitoring.py
+++ monitoring.py
@@ -28,11 +28,10 @@ mydb = client["IAS_PROJECT"]
 # Create a new collection called 'Monitoring' in the 'IAS_PROJECT' database
 monitoringCollection = mydb["Monitoring"]
 ##########################################################
-doc_id = "6432c788b3e40bb758ac7426"
 
 
 ##########    VARIABLE DECLARATIONS    ##########
-newDataDictionary = {}
+newDataDictionary = []
 
 # If the time since the last message received by service is > notifyTime then we'll send a message to the platform Admin via the notification service.
 notifyTime = 15
@@ -41,9 +40,11 @@ notifyTime = 15
 killTime = 30
 
 # To connect to the kafka stream
-kafkaIp = "20.106.92.171"
+# kafkaIp = "20.106.92.171"
+# kafkaPortNo = "9092"
+kafkaIp = "0.0.0.0"
 kafkaPortNo = "9092"
-kafkaTopicName = "testing"
+kafkaTopicName = "heartbeatMonitoring"
 kafkaGroupId = "Monitoring"
 
 # Log should be stored in logFile
@@ -51,51 +52,117 @@ logFile = "Monitoring.log"
 logging.basicConfig(level=logging.WARNING, filename=logFile, filemode='w',
                     format='%(asctime)s - [%(levelname)s] - %(filename)s:%(funcName)s:%(lineno)d - %(message)s')
 
-##########    THREAD FUNCTION IMPLEMENTATION    ##########
+######### Global file ##########
+  
 
+
+##########    THREAD FUNCTION IMPLEMENTATION    ##########
+def UpdateGlobalValue(value):
+    # Connect to python applicaiton
+    new_client = pymongo.MongoClient(
+        "mongodb+srv://test:test@cluster0.xcykxcz.mongodb.net/?retryWrites=true&w=majority")
+    
+    global_val = {
+        "name" : "global_val",
+        "value" : value
+    }
+    # create a new database
+    mydb = new_client["IAS_PROJECT"]
+    db = new_client.test
+    # check if the "SERVER" collection exists
+    if "SERVER" in db.list_collection_names():
+        print()
+        # print("Collection 'SERVER' already exists in 'IAS_PROJECT' database.")
+    else:
+        # create a new collection called "SERVER" within the "IAS_PROJECT" database
+        db.create_collection("SERVER")
+        print("Collection 'SERVER' created successfully in 'IAS_PROJECT' database.")
+
+    # create a new collection called "SERVER" within the "IAS_PROJECT" database
+    serverCollection = mydb["SERVER"]
+    document = serverCollection.find_one({"name": "global_val"})
+    if document is None:
+        #if no such document find
+        result = serverCollection.insert_one(global_val)
+        print("global_value document is inserted")
+    else :
+        serverCollection.update_one({"name": "global_val"}, {"$set": {"value": value}})
+        print(f"Document updated with newvalue = {value} in global")
+
+
+def GetGlobalValue():
+     # Connect to python applicaiton
+    new_client = pymongo.MongoClient(
+        "mongodb+srv://test:test@cluster0.xcykxcz.mongodb.net/?retryWrites=true&w=majority")
+    mydb = new_client["IAS_PROJECT"]
+    
+    db = new_client.test
+    # check if the "SERVER" collection exists
+    if "SERVER" in db.list_collection_names():
+        print()
+        # print("Collection 'SERVER' already exists in 'IAS_PROJECT' database.")
+    else:
+        print("Collection server not found")
+        return 1
+    
+    serverCollection = mydb["SERVER"] 
+    document = serverCollection.find_one({"name": "global_val"})
+    if document is None:
+        #if no such document find
+        print("Document global_val not found")
+        return 1
+    else :
+        return int(document["value"])
+    
 
 def mongoUpdate():
 
     print("Trying to update Mongo")
-    for subsystem in newDataDictionary:
+    newKafkaDictionary = newDataDictionary 
+    print(newDataDictionary)
+    print(newKafkaDictionary)
+
+    for subsystem in newKafkaDictionary:
         #newDataDictinary contains new messages which have to be updated in mongo
         
         # define a filter for the subsystem/doc with the specified name
         filter = {"name": subsystem["name"]}#LoadBalancer_2
 
         #check whether this subsystem already exists
-        existing_subsystem = mycol.find_one(filter)
+        existing_subsystem = monitoringCollection.find_one(filter)
 
         # if the subsystem exists, update its epoctime field
         if existing_subsystem is not None:
-            
             update = {"$set": {"epoc_time": subsystem["epoc_time"]}}
-            mycol.update_one(filter, update)
+            monitoringCollection.update_one(filter, update)
             print("Document updated.")
         # if the document does not exist, insert a new subsystem as new document
         else:
-            mycol.insert_one(subsystem)
+            monitoringCollection.insert_one(subsystem)
             print(f"New subsystem {subsystem['name']} document inserted.")
 
+    newDataDictionary.clear()
         
 
 
 def isalive():
     while (True):
+        sleep(20)
         mongoUpdate()
-        if (int(sys.argv[1]) == global_file.globe):
+        if (int(sys.argv[1]) == GetGlobalValue()):
             # ***********************************************
             '''here notification and updation done on mongodb 
             '''
             #Notification when heartbeat time exceeds 15 and less than 30
             # make filter on the basis of time difference
+            current_time = time.time()
             filter_for_notification = {"$expr": {"$and": [
-                            {"$gt": [{"$subtract": [float(current_time), {"$toDouble": "$epoc_time"}]}, 15]},
-                            {"$lt": [{"$subtract": [float(current_time), {"$toDouble": "$epoc_time"}]}, 30]}
+                            {"$gt": [{"$subtract": [float(current_time), {"$toDouble": "$epoc_time"}]}, 30]},
+                            {"$lt": [{"$subtract": [float(current_time), {"$toDouble": "$epoc_time"}]}, 50]}
                         ]}}
 
             # retrieve all subsystems document that matches the filter
-            subsystems_to_notify = mycol.find(filter_for_notification)
+            subsystems_to_notify = monitoringCollection.find(filter_for_notification)
 
             # initialize a list to store the names of documents/ subsystems
             subsystem_names = []
@@ -103,9 +170,11 @@ def isalive():
             # iterate over the documents and append names of the documents to the list
             for subsystem in subsystems_to_notify:
                 subsystem_names.append(subsystem["name"])
+                print("Platform developer have look at thenm",subsystem["name"],subsystem["containerId"] )
 
             # print the list of document names
-            print(f"Documents with name {subsystem_names} have a time difference between 10 and 20 seconds.")
+            for names in subsystem_names:
+                print(f"Documents with name {names['name']} have a time difference between 10 and 20 seconds.")
 
 
 
@@ -114,42 +183,70 @@ def isalive():
             current_time = time.time()
             
             #make filter on the basis of time exceeding 
-            filter_to_kill = {"$expr": {"$gt": [{"$subtract": [float(current_time), {"$toDouble": "$epoc_time"}]}, 20]}}
+            filter_to_kill = {"$expr": {"$gt": [{"$subtract": [float(current_time), {"$toDouble": "$epoc_time"}]}, 60]}}
             
             # retrieve all subsystems documents that match the filter
-            documents_to_remove = mycol.find(filter_to_kill)
+            documents_to_remove = monitoringCollection.find(filter_to_kill)
 
+            pprint(documents_to_remove)
             # Delete all documents that match the filter
-            result = mycol.delete_many(filter_to_kill) 
+            result = monitoringCollection.delete_many(filter_to_kill) 
 
             # Print the number of documents removed
             print(f"Removed {result.deleted_count} documents.")
 
-            if int(sys.argv[1]) == 1:
-                global_file.globe = 2
-            else:
-                global_file.globe = 1
+            new_val = GetGlobalValue()
+            new_val = new_val ^ 1
+            UpdateGlobalValue(new_val)
 
 
-# t = threading.Thread(target=isalive, args=[])
-# t.start()
+t = threading.Thread(target=isalive, args=[])
+t.start()
 
 
 ##########    MONITORING SUBSYSTEM IMPLEMENTATION    ##########
 
 
-consumer = KafkaConsumer(kafkaTopicName, group_id=kafkaGroupId,
-                         bootstrap_servers=[kafkaIp+":"+kafkaPortNo])
-
+consumer = KafkaConsumer(
+    kafkaTopicName, 
+    group_id=kafkaGroupId,
+    bootstrap_servers=[f"{kafkaIp}:{kafkaPortNo}"])
+print("hi iam running")
 for message in consumer:
-    print(message)
-    messageContents = message.value.decode('UTF-8').split(':')
-    messageContents[0] = messageContents[0][1:]
-    messageContents[2] = messageContents[2][:-1]
-    key = f'{messageContents[0]}:{messageContents[1]}'
-    newDataDictionary[key] = messageContents[2]
-    logging.info('The subsystem = {} with instance id = {} has a new entry'.format(
-        messageContents[0], messageContents[1], messageContents[2]))
+    message_value = message.value.decode('utf-8').strip('"')
+    print("********",message_value)
+    name, containerId, VMhost, VMuname, VMpswd, epoc_time= message_value.split(':')
+    print()
+    print()
+    # problem was newDAtaDictionary appending every time so we are deleteing and end entry 
+    # if same name and containerid present
+    
+    # *******************deleting old entry ***********************************
+    # Check if subsystem with same name and containerId already exists in newDataDictionary
+    subsystem_index = None
+    for index, subsystem in enumerate(newDataDictionary):
+        if subsystem['name'] == name and subsystem['containerId'] == containerId:
+            subsystem_index = index
+            break
+
+    # If subsystem exists, remove it from newDataDictionary, so avoid duplicacy
+    if subsystem_index is not None:
+        del newDataDictionary[subsystem_index]
+    #*******************old endtry deleted ****************************************    
+        
+    #*******************insert new one ********************************************
+    subsystem = {
+        "name": name,
+        "containerId": containerId,
+        "VMhost": VMhost,
+        "VMuname": VMuname,
+        "VMpswd": VMpswd,
+        "epoc_time": epoc_time
+    }
+    newDataDictionary.append(subsystem)
+    #******************new entry inserted *****************************************
+    # logging.info('The subsystem = {} with instance id = {} has a new entry'.format(
+    #     messageContents[0], messageContents[1], messageContents[2]))
     print(newDataDictionary)
 
     
