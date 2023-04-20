bda6aeb235ec845be51c2257179b8dbc55a659a0
Yash Sampat <yashsampat23154@gmail.com>
2023-04-20 21:58:44 +0530

    Major changes

--- monitoring.py
+++ monitoring.py
@@ -1,16 +1,11 @@
 # How to run : 
 
-# Command : python3 monitoring.py <a> <b>
-# a = Subsystem instance we wish to run. 
-# b = Logging topic name on which we wish to log 
+# Command : python3 monitoring.py
 
 # Eg : 
 
 # For logging the first istance of monitoring we'll write : 
-# python3 monitoring.py 1 log-Monitoring-1 
-
-# For logging the second istance of monitoring we'll write : 
-# python3 monitoring.py 2 log-Monitoring-2
+# python3 monitoring.py 
 
 
 from kafka import KafkaConsumer
@@ -19,8 +14,10 @@ import json
 import threading
 import time
 from time import sleep
-import sys
-from pprint import pprint
+from kafka.errors import KafkaError
+import random
+from loggingMessage import *
+
 
 
 ########### For Mongo Database ############################
@@ -34,201 +31,138 @@ client = pymongo.MongoClient("mongodb+srv://test:test@cluster0.xcykxcz.mongodb.n
 # Creating a new database
 mydb = client["IAS_PROJECT"]
 
-# Create a new collection called 'Monitoring' in the 'IAS_PROJECT' database
-monitoringCollection = mydb["Monitoring"]
+# Create a new collection called "standardMonitoring" and "alertedMonitoring" in the 'IAS_PROJECT' database
+standardMonitoringCollection = mydb["standardMonitoring"]
+alertedMonitoringCollection = mydb["alertedMonitoring"]
 
-##########################################################
 
 
 ##########    VARIABLE DECLARATIONS    ##########
-newDataDictionary = []
 
 # If the time since the last message received by service is > notifyTime then we'll send a message to the platform Admin via the notification service.
-notifyTime = 30
+notifyTime = 250
 
 # If the time since the last message received by service is > killTime then we'll pass the service along with its information to the Fault Tolerance system which will deal with it.
-killTime = 60
+killTime = 300
 
 # To connect to the kafka stream
 # kafkaIp = "20.106.92.171" # IP of external globalValue where kafka is supposed to run.
 kafkaIp = "0.0.0.0"
 kafkaPortNo = "9092"
-kafkaTopicName = "heartbeatMonitoring"
+kafkaTopicName = ["heartbeat-monitoring", "heartbeat-fault-tolerance", "heartbeat-deployer", "heartbeat-sensor-manager", "heartbeat-load-balancer", "heartbeat-node-manager", "heartbeat-scheduler", "heartbeat-validator-workflow", "heartbeat-developer"]
 kafkaGroupId = "Monitoring"
 
-
 # Setting up logging credentials : 
-loggingTopicName = sys.argv[2]
 producerForLogging = KafkaProducer(bootstrap_servers=[kafkaIp+":"+kafkaPortNo],api_version=(0, 10, 1))
+
 
 
 ##########    THREAD FUNCTION IMPLEMENTATION    ##########
 
-def UpdateGlobalValue(value):
+# Function for monitoring the containers that haven't send their heartbeat for more than 250 seconds. In case these systems don't sent a heartbeat for 500 seconds then they are to be declared dead and their container has to be restarted.
 
-    global_val = {
-        "name" : "global_val",
-        "value" : value
-    }
-
-    globalValueCollection = mydb["globalValue"]
-
-    document = globalValueCollection.find_one({"name": "global_val"})
-    if document is None:
-        globalValueCollection.insert_one(global_val)
-    else :
-        globalValueCollection.update_one({"name": "global_val"}, {"$set": {"value": value}})
-       
-
-
-def GetGlobalValue():
-
-    if "globalValue" not in mydb.list_collection_names():
-        return 1
-
-    globalValueCollection = mydb["globalValue"] 
-    document = globalValueCollection.find_one({"name": "global_val"})
-    return int(document["value"])
-    
-    
-
-def mongoUpdate():
-    
-    # print(newDataDictionary)
-    # print(newKafkaDictionary)
-
-    for subsystem in newDataDictionary.copy():
-
-        # NewDataDictinary contains new messages which have to be updated in mongo
-        
-        # Define a filter for the subsystem/doc with the specified name
-        filter = {"name": subsystem["name"], "containerId": subsystem["containerId"]}
-
-        # Check whether this subsystem already exists
-        existing_subsystem = monitoringCollection.find_one(filter)
-
-        # If the subsystem exists, update its epoctime field
-        if existing_subsystem is not None:
-            update = {"$set": {"epoc_time": subsystem["epoc_time"]}}
-            monitoringCollection.update_one(filter, update)        
-
-        # If the document does not exist, insert a new subsystem as new document if its time stamp is less than kill time.
-        else:
-            if float(time.time()) - float(subsystem["epoc_time"]) < killTime : 
-                monitoringCollection.insert_one(subsystem)
-            else : 
-                newDataDictionary.remove(subsystem)
-    
-
-
-def isalive():
+def alertedMonitoring():
 
     while (True):
 
-        sleep(20)
+        # Made filter on the basis of time difference
+        current_time = time.time()
+        filter_to_kill = {"$expr": {"$gt": [{"$subtract": [float(current_time), {"$toDouble": "$current_time"}]}, killTime]}}
+        
+        # Retrieve all subsystems documents that match the filter
+        documents_to_remove = alertedMonitoringCollection.find(filter_to_kill)
 
-        mongoUpdate()
-
-        if (int(sys.argv[1]) == GetGlobalValue()):
+        # Iterating over the documents that will be send to the fault tolerance system to be deleted. 
+        for subsystem in documents_to_remove:
+            log_message("monitoring", "CRITICAL", "The susbsystem having container name - {} and node name - {} has be scheduled to be deleted.".format(subsystem["container_name"], subsystem["node_name"]))
+            alertedMonitoringCollection.delete_one(subsystem)
             
-            # Notification when heartbeat time exceeds 150 and less than 300
-            # Made filter on the basis of time difference
-            current_time = time.time()
-            filter_for_notification = {"$expr": {"$and": [
-                {"$gt": [{"$subtract": [float(current_time), {"$toDouble": "$epoc_time"}]}, notifyTime]},
-                {"$lt": [{"$subtract": [float(current_time), {"$toDouble": "$epoc_time"}]}, killTime]}
-            ]}}
+            # TODO : Send all these subsystems to the Fault Tolerance subsystem to kill them. 
 
-            # Retrieve all subsystems document that matches the filter
-            subsystems_to_notify = monitoringCollection.find(filter_for_notification)
+        sleep(30)
 
-            # Initialize a list to store the names of documents/ subsystems
-            subsystem_names = []
+        
 
-            # Iterate over the documents and append names of the documents to the list
-            for subsystem in subsystems_to_notify:
-                subsystem_names.append(subsystem["name"])
+# Function for monitoring the containers that haven't send their heartbeat for more than 250 seconds. In case these systems don't sent a heartbeat for 250 seconds then they are to be shifted with the alerted monitoring db and a notification has to be sent to the platform admin that this particular subsystem isn't acting well. 
 
-                message = "WARNING:"+"The susbsystem - {}, having container id - {}, hasn't responded in some time.".format(subsystem["name"], subsystem["containerId"])
-                producerForLogging.send(loggingTopicName, json.dumps(message).encode('utf-8'))
+def standardMonitoring():
 
-                # print("Platform developer have look at them",subsystem["name"],subsystem["containerId"] )
+    sleep(random.randint(0, 60))
 
+    # print("Will finally enter the standard Monitoring code")
 
-            # Iterate and remove
-            current_time = time.time()
+    while (True):
             
-            # Make filter on the basis of time exceeding 
-            filter_to_kill = {"$expr": {"$gt": [{"$subtract": [float(current_time), {"$toDouble": "$epoc_time"}]}, killTime]}}
-            
-            # Retrieve all subsystems documents that match the filter
-            documents_to_remove = monitoringCollection.find(filter_to_kill)
+        # Notification when heartbeat time exceeds 150 and less than 300
+        
+        current_time = time.time()
+        filter_for_notification = {"$expr": {"$gt": [{"$subtract": [float(current_time), {"$toDouble": "$current_time"}]}, notifyTime]}}
+
+        # Retrieve all subsystems document that matches the filter
+        subsystems_to_notify = standardMonitoringCollection.find(filter_for_notification)
+
+        # Initialize a list to store the container_name of documents/ subsystems
+        subsystem_names = []
+
+        # Iterate over the documents and append container_name of the documents to the list
+        for subsystem in subsystems_to_notify:
+            subsystem_names.append(subsystem["container_name"])
+            log_message("monitoring", "WARNING", "The susbsystem having container name - {} and node name - {} hasn't responded in some time.".format(subsystem["container_name"], subsystem["node_name"]))
+            alertedMonitoringCollection.insert_one(subsystem)
+            standardMonitoringCollection.delete_one(subsystem)
+
+            # TODO : Send message to the platform admin telling him that this particular node hasn't responded in quiet some time.
+        
+        sleep(60)
 
 
-            # Iterating over the documents that will be send to the fault tolerance system to be deleted. 
-            for subsystem in documents_to_remove:
-
-                message = "CRITICAL:" + "The susbsystem - {}, having container id - {}, has be scheduled to be deleted.".format(subsystem["name"], subsystem["containerId"])
-                producerForLogging.send(loggingTopicName, json.dumps(message).encode('utf-8'))
-                
-                # TODO : Send all these subsystems to the Fault Tolerance subsystem to kill them. 
 
 
-            # pprint(documents_to_remove)
-            # Delete all documents that match the filter
-            result = monitoringCollection.delete_many(filter_to_kill)
+t1 = threading.Thread(target=standardMonitoring, args=[])
+t2 = threading.Thread(target=alertedMonitoring, args=[])
+t1.start()
+t2.start()
 
-            # # Print the number of documents removed
-            # print(f"Removed {result.deleted_count} documents.")
-
-
-            new_val = GetGlobalValue()
-            new_val = new_val ^ 3
-            UpdateGlobalValue(new_val)
-
-
-t = threading.Thread(target=isalive, args=[])
-t.start()
 
 
 ##########    MONITORING SUBSYSTEM IMPLEMENTATION    ##########
 
+def startMonitoring(kafkaTopicName): 
 
-consumer = KafkaConsumer(kafkaTopicName, group_id=kafkaGroupId, bootstrap_servers=[f"{kafkaIp}:{kafkaPortNo}"]) 
+    consumer = KafkaConsumer(kafkaTopicName, group_id=kafkaGroupId, bootstrap_servers=[f"{kafkaIp}:{kafkaPortNo}"]) 
 
-for message in consumer:
-    
-    message_value = message.value.decode('utf-8').strip('"')
-    
-    name, containerId, VMhost, VMuname, VMpswd, epoc_time = message_value.split(':')
-    
-    # Problem was newDataDictionary appending every time so we are deleteing and end entry 
-    # If same name and containerid present
-    
-    # ******************* Deleting Old Entry ***********************************
-    
-    # Check if subsystem with same name and containerId already exists in newDataDictionary
-    subsystem_index = None
-    for index, subsystem in enumerate(newDataDictionary):
-        if subsystem['name'] == name and subsystem['containerId'] == containerId:
-            subsystem_index = index
-            break
+    try:
 
-    # If subsystem exists, remove it from newDataDictionary, so avoid duplicacy
-    if subsystem_index is not None:
-        del newDataDictionary[subsystem_index]
-   
+        for message in consumer:
         
-    #******************* Insert New One ********************************************
+            messageContents = json.loads(message.value)
 
-    subsystem = {
-        "name": name,
-        "containerId": containerId,
-        "VMhost": VMhost,
-        "VMuname": VMuname,
-        "VMpswd": VMpswd,
-        "epoc_time": epoc_time
-    }
-    newDataDictionary.append(subsystem)
+            # print(messageContents)
+            
+            # Define a filter for the subsystem/doc with the specified name
+            filter = {"_id": messageContents["container_name"]} # Takes O(log n)
 
-    
+            # Check whether this subsystem already exists
+            existing_subsystem = standardMonitoringCollection.find_one(filter)
+
+            # If the subsystem exists, update its current_time field
+            if existing_subsystem is not None:
+                update = {"$set": {"current_time": messageContents["current_time"]}}
+                standardMonitoringCollection.update_one(filter, update)        
+
+            # If the document does not exist, insert a new subsystem as new document if its time stamp is less than kill time.
+            else : 
+                messageContents["_id"] = messageContents["container_name"]
+                standardMonitoringCollection.insert_one(messageContents)
+            
+        
+    except KeyboardInterrupt:
+        pass
+
+    except KafkaError as kError:
+        print(f'Error while consuming messages from topics {kafkaTopicName}: {kError}')
+
+
+for topic in kafkaTopicName :
+    t = threading.Thread(target=startMonitoring, args=(topic, ))
+    t.start()
