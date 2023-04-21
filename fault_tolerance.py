25a73504f4fee071826c3b5d27febcfe6367b012
SanyaGandhi <gandhisanya99@gmail.com>
2023-04-21 12:39:09 +0530

    Updated fault tolerance file with exception handling

--- fault_tolerance.py
+++ fault_tolerance.py
@@ -1,5 +1,10 @@
 import os
+import pymongo
+import loggingMessage as logfile
 
+client_nodeInfo = pymongo.MongoClient("mongodb+srv://root:rp123123@lab8.qnzi7qu.mongodb.net/?retryWrites=true&w=majority")
+mydb = client_nodeInfo["IAS"]
+vmCollection = mydb["nodeDB"]
 
 #VM details needed from the caller:
 #1. vm password
@@ -7,18 +12,38 @@ import os
 #3. vm ip
 #4. Container id/name needed
 
-#node_id is the key linked to the
+#node_id is the key linked to the 1,2,3
 def reinitiate_container(container_name, node_id):
-    vm_name=node_id #this needs to be extracted from the database that stores the names, ips and passwords of all the vms/nodes
-    vm_ip='20.193.154.7' #this again needs to be extracted from the same db
-    vm_pswd='IASiotplatform@123' #same as above
-    commands = list()
-    commands.append(f"docker stop {container_name}")
-    commands.append(f"docker start {container_name}")
-    commands.append("docker ps -a")
-    commands.append("exit")
-    command = ';'.join(commands)
 
-    os.system(f"sshpass -p {vm_pswd} ssh {vm_name}@{vm_ip} '" + command + "'")
+    filter = {"node_name": node_id} # Takes O(log n)
 
-reinitiate_container('1301ce5c4730','iasvm1') #just for testing purposes.. The specified container id made to run
+    # Check whether this entry exists
+    nodeInfo = vmCollection.find_one(filter)
+
+    # If the entry exists, get the values
+    if nodeInfo is not None:
+        vm_name = nodeInfo["user_name"]
+        vm_ip = nodeInfo["ip"]
+        vm_pswd = nodeInfo["password"]
+        commands = list()
+        commands.append(f"docker stop {container_name}")
+        commands.append(f"docker start {container_name}")
+        commands.append("docker ps -a")
+        commands.append("exit")
+        command = ';'.join(commands)
+        try:
+            os.system(f"sshpass -p {vm_pswd} ssh {vm_name}@{vm_ip} '" + command + "'")
+        except:
+            log_message=f' Unable to run scripts at node with {node_id} is not found'
+            print(log_message)
+            logfile.log_message('fault-tolerance','ERROR',log_message)
+
+    # If the document does not exist.
+    else : 
+        log_message=f' The entry in DB for node with {node_id} is not found'
+        print(log_message)
+        logfile.log_message('fault-tolerance','ERROR',log_message)
+
+
+
+reinitiate_container('cont_ldap','sid_node') #just for testing purposes.. The specified container id made to run
